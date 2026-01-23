ca25598d496f4fa58bef5592c62306117902f8b5
Luis Quental Almeida <luis.quental.almeida@gmail.com>
2026-01-23 19:10:35 +0000

    Initialize Mandatory Part: philo
    - Create folder structure with initial files:
    	- philo/Makefile
    	- philo/src/philo.c
    	- philo/include/philo.h
    
    - Create initial Makefile with correct include path and src/philo.c.
    - Create initial header file (philo.h) with all includes for all allowed fucntions.
    	- Later remove unused includes and comments.
    - Create initial main file (philo.c) with include for philo.h.

---
+++ Philosophers/philo/include/philo.h
@@ -0,0 +1,12 @@
+#ifndef PHILO_H
+# define PHILO_H
+
+# include <string.h> //memset
+# include <stdio.h> //printf
+# include <stdlib.h> //malloc, free
+# include <unistd.h> //write, usleep
+# include <sys/time.h> //gettimeofday
+# include <pthread.h> /*p_thread_create, pthread_detach, pthread_join, pthread_mutex_init,
+			p_thread_mutex_destroy, pthread_mutex_lock, pthread_mutex_unlock*/
+
+#endif
