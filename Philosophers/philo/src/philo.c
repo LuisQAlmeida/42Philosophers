44387a3091903878ebb9e18e84129894c22e209f
Luis Quental Almeida <luis.quental.almeida@gmail.com>
2026-05-22 22:31:39 +0100

    chore(philo): bootstrap main

--- Philosophers/philo/src/philo.c
+++ Philosophers/philo/src/philo.c
@@ -1,1 +1,13 @@
 #include "philo.h"
+
+int	main(int argc, char **argv)
+{
+	(void)argv;
+	if (argc != 5 && argc != 6)
+	{
+		printf("You should insert either 4 or 5 arguments. No more, no less...\n");
+		return (1);
+	}
+	printf("You've inserted the right amount of arguments!\n");
+	return (0);
+}
