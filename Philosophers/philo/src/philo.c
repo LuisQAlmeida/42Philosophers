7341b892f71216a37a322b7ce9fbbe9ea078ba41
Luis Quental Almeida <luis.quental.almeida@gmail.com>
2026-05-26 22:31:29 +0100

    feat(philo): use parse_rules in main

--- Philosophers/philo/src/philo.c
+++ Philosophers/philo/src/philo.c
@@ -2,12 +2,19 @@
 
 int	main(int argc, char **argv)
 {
-	if (!parse_args(argc, argv))
+	t_rules	rules;
+
+	if (!parse_rules(&rules, argc, argv))
 	{
 		printf("Invalid arguments.\n");
 		printf("Usage: ./philo n_philo t_die t_eat t_sleep [must_eat]\n");
 		return (1);
 	}
-	printf("Valid arguments inserted: positive integers.\n");
+	printf("n_philo = %d | t_die = %ld | t_eat = %ld | t_sleep = %ld ", 
+			rules.n_philo, rules.t_die, rules.t_eat, rules.t_sleep);
+	if (rules.has_must_eat)
+		printf("| must_eat = %d\n", rules.must_eat);
+	else
+		printf("\n");
 	return (0);
 }
