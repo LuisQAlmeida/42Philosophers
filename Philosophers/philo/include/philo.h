c77100769c0a4698b647a7dd21e7799b038dccb1
Luis Quental Almeida <luis.quental.almeida@gmail.com>
2026-05-26 13:41:00 +0100

    feat(philo): introduce t_rules and parse_rules

--- Philosophers/philo/include/philo.h
+++ Philosophers/philo/include/philo.h
@@ -9,8 +9,19 @@
 # include <pthread.h> /*p_thread_create, pthread_detach, pthread_join, pthread_mutex_init,
 			p_thread_mutex_destroy, pthread_mutex_lock, pthread_mutex_unlock*/
 
+typedef struct s_rules
+{
+	int		n_philo;
+	long	t_die;
+	long	t_eat;
+	long	t_sleep;
+	int		must_eat;
+	int		has_must_eat;
+}	t_rules;
+
 int		ft_isdigit(int c);
 long	ft_atol_positive(const char *s, int *ok);
 int		parse_args(int ac, char **av);
+int		parse_rules(t_rules *rules, int ac, char **av);
 
 #endif
--- Philosophers/philo/src/parse.c
+++ Philosophers/philo/src/parse.c
@@ -1,4 +1,29 @@
+#include <limits.h>
 #include "philo.h"
+
+static int	set_long(long *dst, const char *src)
+{
+	int		ok;
+	long	value;
+
+	value = ft_atol_positive(src, &ok);
+	if (!ok)
+		return (0);
+	*dst = value;
+	return (1);
+}
+
+static int	set_int(int *dst, const char *src)
+{
+	int		ok;
+	long	value;
+
+	value = ft_atol_positive(src, &ok);
+	if (!ok || value > INT_MAX)
+		return (0);
+	*dst = (int)value;
+	return (1);
+}
 
 int	parse_args(int ac, char **av)
 {
@@ -15,6 +40,28 @@ int	parse_args(int ac, char **av)
 		if (!ok || value <= 0)
 			return (0);
 		i++;
+	}
+	return (1);
+}
+
+int	parse_rules(t_rules *rules, int ac, char **av)
+{
+	memset(rules, 0, sizeof(*rules));
+	if (ac != 5 && ac != 6)
+		return (0);
+	if (!set_int(&rules->n_philo, av[1]))
+		return (0);
+	if (!set_long(&rules->t_die, av[2]))
+		return (0);
+	if (!set_long(&rules->t_eat, av[3]))
+		return (0);
+	if (!set_long(&rules->t_sleep, av[4]))
+		return (0);
+	if (ac == 6)
+	{
+		rules->has_must_eat = 1;
+		if (!set_int(&rules->must_eat, av[5]))
+			return (0);
 	}
 	return (1);
 }
