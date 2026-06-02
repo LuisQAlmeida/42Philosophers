e5e483bfa863d9e133252721630b03a9ed07ea0c
Luis Quental Almeida <luis.quental.almeida@gmail.com>
2026-06-02 23:20:36 +0100

    fix(philo): avoid actions after stop

--- Philosophers/philo/src/philo_actions.c
+++ Philosophers/philo/src/philo_actions.c
@@ -2,7 +2,14 @@
 
 void	philo_eat(t_philo *philo)
 {
+	if (get_stop(philo->dinner))
+		return ;
 	take_forks(philo);
+	if (get_stop(philo->dinner))
+	{
+		drop_forks(philo);
+		return ;
+	}
 	update_meal_state(philo);
 	log_philo(philo, "is eating");
 	smart_sleep(philo->dinner, philo->dinner->rules.t_eat);
@@ -11,12 +18,16 @@ void	philo_eat(t_philo *philo)
 
 void	philo_sleep(t_philo *philo)
 {
+	if (get_stop(philo->dinner))
+		return ;
 	log_philo(philo, "is sleeping");
 	smart_sleep(philo->dinner, philo->dinner->rules.t_sleep);
 }
 
 void	philo_think(t_philo *philo)
 {
+	if (get_stop(philo->dinner))
+		return ;
 	log_philo(philo, "is thinking");
 }
 
